affb2c00c45128d1920dfa33f5e5a487a827309a
Giovanni Casu <giovanni.casu@gmail.com>
2022-08-10 12:14:39 +0200

    merge

--- migrations/3_initial_migration.js
+++ migrations/3_initial_migration.js
@@ -2,7 +2,7 @@ var tcr = artifacts.require("Tcr");
 var token = artifacts.require("Token");
 
 module.exports = function(deployer) {
-  deployer.deploy(tcr, "Demo Guild", "Testing environment for the guild application, everyone is well accepted as applicant", "0x15A493e00A58e42CeB27F22119cc1Dc7c87A2321", [100, 60, 60]); // token.address
+  deployer.deploy(tcr, "Best of legal / tech advisors (Italia)", "Best of legal / tech advisors (Italia)", "0x568adfa602e8ba1bef461d7b7776179ee810257e", [100, 600, 600]); // token.address
   // deployer.deploy(tcr, "DemoTcr", "0x81b799Fd5e681B788844d71c26e85Aab41e609D4", [100, 60, 60]); // new solidity
   // deployer.deploy(tcr, "BestGuildsTesters", "0x00Bfb053c776AD912FE3aA3Cc051C2488CD51a77", [100, 259200, 259200]);
 };
